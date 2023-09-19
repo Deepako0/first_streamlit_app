2baeeac804d10cd7d3da7c21b79c609756e593c2
Deepako0 <57000077+Deepako0@users.noreply.github.com>
2023-09-19 16:37:42 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,3 +23,7 @@ streamlit.header('Fruityvice Fruit Advice!')
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 streamlit.text(fruityvice_response.json())
 
+# Normalizes the data into a table
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+# Turned into a Panda Dataframe
+streamlit.dataframe(fruityvice_normalized)

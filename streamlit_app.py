26c9d8cd93a6de4f3248f2291dcc7acb66688861
Deepako0 <57000077+Deepako0@users.noreply.github.com>
2023-09-19 16:34:35 -0700

    Update streamlit_app.py
    Response ----> Json()

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,7 @@ import pandas
 import requests
 
 
-streamlit.title(' My Parents New Healthy Diner')
+streamlit.title(' My Mom's New Healthy Diner')
 streamlit.header('Breakfast Favourites')
 
 
@@ -18,6 +18,8 @@ my_fruit_list = my_fruit_list.set_index('Fruit')
 fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado', 'Strawberries'])
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
+
+streamlit.header('Fruityvice Fruit Advice')
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response)
+streamlit.text(fruityvice_response.json())
 

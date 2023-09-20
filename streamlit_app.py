12a6c24f0df7390debf9eef825af409306e9784f
Deepako0 <57000077+Deepako0@users.noreply.github.com>
2023-09-20 15:04:38 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,6 +26,7 @@ fruit_choice = streamlit.text_input('What fruit would you like information about
 streamlit.write('The user entered ', fruit_choice)
 
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/kiwi")
+streamlit.stop()
 streamlit.text(fruityvice_response.json())
 
 
